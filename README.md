f59e6219800165d3b6641fcd37d316f87f1611ba
Expolite <macompas.ronnel@gmail.com>
2024-02-21 15:06:52 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# color codes
+* bg-white: #F5F5F5;
+* brown-1: #65351F
+* brown-2: #765827
+* coffee: #3F2305
+
+* header-dark: #2F2F2F
+* header-description: #64748B
+
+
+# shadow-use
+- box-shadow: 0 5px 15px rgba(101, 69, 31, 1);
---
+++ assets/js/mainScript.js
@@ -0,0 +1,15 @@
+
+
+setTimeout(function(){
+    flex_01_left_header_fade.style.left = "0";
+    flex_01_left_header_fade.style.opacity = "1";
+},700);
+
+
+setTimeout(function(){
+    flex_01_left_desc_fade.style.left = "0";
+    flex_01_left_desc_fade.style.opacity = "1";
+},700);
+
+
+
